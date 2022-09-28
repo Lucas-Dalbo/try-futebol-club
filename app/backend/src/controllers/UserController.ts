6ae871210b06984b448c9c33db8c8cb4fc9d0ff1
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-28 14:37:31 -0300

    coloca await

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -10,7 +10,7 @@ class UserController {
 
   public async login(req: Request, res: Response) :Promise<void> {
     const { email, password } = req.body;
-    const user = this._service.login(email, password);
+    const user = await this._service.login(email, password);
     res.status(200).json(user);
   }
 }
