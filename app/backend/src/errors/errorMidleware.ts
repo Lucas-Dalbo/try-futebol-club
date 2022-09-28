5a06e9849c1b7fe53ed79f54bfe46e49081a20e1
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-28 14:58:33 -0300

    feat: erros custmomizados

---
+++ app/backend/src/errors/errorMidleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import CustomError from './CustomError';
+
+const errorMiddleware = (err: CustomError, _req: Request, res: Response, _next: NextFunction) => {
+  if (!err.status) {
+    console.log(err);
+    return res.status(500).json({ message: 'Something went wrong' });
+  }
+
+  const { message, status } = err;
+  res.status(status).json({ message });
+};
+
+export default errorMiddleware;
