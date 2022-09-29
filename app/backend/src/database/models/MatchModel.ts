3bbb3a886f953f1761903b4462799c7c9bcf2fa5
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-29 15:14:24 -0300

    feat: Migration e Model p/ Matches

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,51 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Team from './TeamModel';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Match.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+Team.belongsTo(Match, { as: 'matches', foreignKey: 'homeTeam' });
+Team.belongsTo(Match, { as: 'matches', foreignKey: 'awayTeam' });
+
+Match.hasMany(Team, { as: 'teams', foreignKey: 'homeTeam' });
+Match.hasMany(Team, { as: 'teams', foreignKey: 'awayTeam' });
