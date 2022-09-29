c6dd4a54c9374f79ebdddc1a10f6f433331a7f62
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-29 09:54:49 -0300

    refactor: UserService - Refatora devido a mudança nas interfaces

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,5 +1,5 @@
 import * as bcrypt from 'bcryptjs';
-import { IUser } from '../interfaces';
+import { IUser, LoginUser } from '../interfaces';
 import CustomError from '../errors/CustomError';
 import UserModel from '../database/models/UserModel';
 
@@ -20,7 +20,9 @@ class UserService {
     return user as IUser;
   }
 
-  public async login(email: string, password: string): Promise<void> {
+  public async login(data: LoginUser): Promise<void> {
+    const { email, password } = data;
+
     const user = await this.emailValidation(email);
 
     const passVerify = bcrypt.compareSync(password, user.password);
