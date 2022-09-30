9a6f3a64ca85d5fe0866fb356f4bdbca70d97e05
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-30 10:12:14 -0300

    test: Rota GET /matches - Adiciona testes relacionados a query string

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -19,15 +19,55 @@ describe('A rota GET /matches', () => {
 
     after(() => {
       (Match.findAll as sinon.SinonStub).restore();
+    });
+
+    it('Retorna uma lista de matches com status 200', async () => {
+      const result = await chai.request(app).get('/matches');
+  
+      expect(result.body).to.be.an('array');
+      expect(result.status).to.be.equal(200);
     })
-  })
+  });
 
-  it('Retorna uma lista de matches com status 200', async () => {
-    const result = await chai.request(app).get('/matches');
+  describe('Ao ser chamada com ?inProgress=true', () => {
+    before(() => {
+      sinon.stub(Match, 'findAll').resolves([mockMatches[1]] as unknown as Match[]);
+    })
 
-    expect(result.body).to.be.an('array');
-    expect(result.status).to.be.equal(200);
-  })
+    after(() => {
+      (Match.findAll as sinon.SinonStub).restore();
+    });
+  
+    it('Retorna uma lista de matches "inProgress = true" com status 200', async () => {
+      const result = await chai.request(app).get('/matches?inProgress=true');
+  
+      const everyProgressIsTrue = result.body.every((match: { inProgress: boolean; }) => match.inProgress === true);
+  
+      expect(result.body).to.be.an('array');
+      expect(everyProgressIsTrue).to.be.equal(true);
+      expect(result.status).to.be.equal(200);
+    });
+  });
+
+  describe('Ao ser chamada com ?inProgress=true', () => {
+    before(() => {
+      sinon.stub(Match, 'findAll').resolves([mockMatches[0]] as unknown as Match[]);
+    })
+
+    after(() => {
+      (Match.findAll as sinon.SinonStub).restore();
+    });
+  
+    it('Retorna uma lista de matches "inProgress = true" com status 200', async () => {
+      const result = await chai.request(app).get('/matches?inProgress=false');
+  
+      const everyProgressIsFalse = result.body.every((match: { inProgress: boolean; }) => match.inProgress === false);
+  
+      expect(result.body).to.be.an('array');
+      expect(everyProgressIsFalse).to.be.equal(true);
+      expect(result.status).to.be.equal(200);
+    });
+  });
 
   describe('Quando ocorre um erro interno', () => {
     before(() => {
