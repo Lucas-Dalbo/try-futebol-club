5a94154ceef75acf579b3db878326ae0dde28165
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-01 14:07:07 -0300

    feat: rotas /leaderboard

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import UserRoute from './routes/UserRoute';
 import TeamRoute from './routes/TeamRoute';
 import MatchRoute from './routes/MatchRouter';
+import LeaderboardRoute from './routes/LeaderboardRoute';
 import errorMiddleware from './errors/errorMidleware';
 
 class App {
@@ -14,10 +15,8 @@ class App {
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
-    this.app.use('/login', UserRoute.route);
-    this.app.use('/teams', TeamRoute.route);
-    this.app.use('/matches', MatchRoute.route);
-    this.app.use(errorMiddleware);
+
+    this.setRoutes();
   }
 
   private config():void {
@@ -30,6 +29,14 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+  }
+
+  private setRoutes(): void {
+    this.app.use('/login', UserRoute.route);
+    this.app.use('/teams', TeamRoute.route);
+    this.app.use('/matches', MatchRoute.route);
+    this.app.use('/leaderboard', LeaderboardRoute.route);
+    this.app.use(errorMiddleware);
   }
 
   public start(PORT: string | number):void {
